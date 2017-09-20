b8cdd49831ab17d64d3b934163afe870119fbd71
League <League@DESKTOP-UV1N0JE>
2017-09-19 19:23:29 -0700

    Tacos are good

---
+++ src/EverythingisAwesome.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class EverythingisAwesome {
+	public static void main(String[] args) {
+		String like= JOptionPane.showInputDialog("What do you like?"); 
+		JOptionPane.showMessageDialog(null,like+ " are awesome");
+	}
+	
+	
+	
+	
+	
+	
+	
+}
