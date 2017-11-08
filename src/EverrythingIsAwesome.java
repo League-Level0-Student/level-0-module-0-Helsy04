3de1c149dcf74ce99605c5d3476fe2f18d456086
CatcusCat <wjmorris2004@gmail.com>
2017-11-07 19:24:27 -0800

    EverythingIsAwesome GreeterRecipe KnockKnock

---
+++ src/EverrythingIsAwesome.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class EverrythingIsAwesome {
+public static void main(String[] args) {
+	String like= JOptionPane.showInputDialog("What do you like?"); 
+	JOptionPane.showMessageDialog(null,like+ " are awesome");
+}
+}
+
--- src/EverythingisAwesome.java
+++ src/EverythingisAwesome.java
@@ -3,6 +3,12 @@ import javax.swing.JOptionPane;
 public class EverythingisAwesome {
 	public static void main(String[] args) {
 		String like= JOptionPane.showInputDialog("What do you like?"); 
+		
+		
+		
+		
+		
+		
 		JOptionPane.showMessageDialog(null,like+ " are awesome");
 	}
 	
